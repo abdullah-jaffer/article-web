58220d6b3918ddd63cf06a70233f5b4c2cf3db69
abdullah-jaffer <abdullahjaffer96@gmail.com>
2024-04-17 16:20:51 +0500

    added page files

---
+++ app/article-screen/page.tsx
@@ -0,0 +1,10 @@
+
+export default function MainScreen() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Your on the article</h1>
+
+
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,151 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+export default async function ArticleScreen() {
+  const articlesList = await fetch(`https://ttk3k6u951.execute-api.us-east-1.amazonaws.com/Prod/articles`, {
+    next: { revalidate: 10 },
+  })
+  const articles = await articlesList.json();
+
+    return (
+      <div className=" bg-blue-50">
+        <div className="p-10 text-center bg-black text-slate-100 text-9xl">
+           <h1 className="text-9xl">Hi, my name is Abdullah</h1>
+           <h2 className="text-6xl mt-10">Read my thoughts below</h2>
         </div>
-      </div>
+        <div>
+        <h2 className="pl-10 text-6xl mt-8">My old Work</h2>
+        <div className="p-10 flex flex-wrap -mx-4 ">
+        <div className="w-full max-w-full mb-8 sm:w-1/2 px-4 lg:w-1/3 flex flex-col">
+            <img src="https://source.unsplash.com/Lki74Jj7H-U/400x300" alt="Card img" className="object-cover object-center w-full h-48" />
+            <div className="flex flex-grow">
+                <div className="triangle"></div>
+                <div className="flex flex-col justify-between px-4 py-6 bg-white border border-gray-400 text">
+                    <div>
+                       
+                        <a href="#"
+                            className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
+                            What Zombies Can Teach You About Food
+                        </a>
+                    
+                    </div>
+                    <div>
+                        <a href="#"
+                            className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
+                            More -></a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="w-full max-w-full mb-8 sm:w-1/2 px-4 lg:w-1/3 flex flex-col">
+            <img
+            src="https://source.unsplash.com/L9_6GOv40_E/400x300"
+            alt="Card img"
+            className="object-cover object-center w-full h-48"
+          />
+            <div className="flex flex-grow">
+                <div className="triangle"></div>
+                <div className="flex flex-col justify-between px-4 py-6 bg-white border border-gray-400">
+                    <div>
+                       
+                        <a href="#"
+                            className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
+                            Old School Art
+                        </a>
+                      
+                    </div>
+                    <div>
+                        <a href="#"
+                            className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
+                            More -></a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+        <div className="w-full max-w-full mb-8 sm:w-1/2 px-4 lg:w-1/3 flex flex-col">
+            <img
+            src="https://source.unsplash.com/7JX0-bfiuxQ/400x300"
+            alt="Card img"
+            className="object-cover object-center w-full h-48"
+          />
+            <div className="flex flex-grow">
+                <div className="triangle"></div>
+                <div className="flex flex-col justify-between px-4 py-6 bg-white border border-gray-400">
+                    <div>
+                       
+                        <a href="#"
+                            className="w-full block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
+                            5 Things To Do About Rain
+                        </a>
+                      
+                    </div>
+                    <div>
+                        <a href="#"
+                            className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
+                            More -></a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div className="w-full max-w-full mb-8 sm:w-1/2 px-4 lg:w-1/3 flex flex-col">
+            <img
+            src="https://source.unsplash.com/7JX0-bfiuxQ/400x300"
+            alt="Card img"
+            className="object-cover object-center w-full h-48"
+          />
+            <div className="flex flex-grow">
+                <div className="triangle"></div>
+                <div className="flex flex-col justify-between px-4 py-6 bg-white border border-gray-400">
+                    <div>
+                       
+                        <a href="#"
+                            className="w-full block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
+                            5 Things To Do About Rain
+                        </a>
+                      
+                    </div>
+                    <div>
+                        <a href="#"
+                            className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
+                            More -></a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+        </div>
+        <div>
+        <h2 className="pl-10 text-6xl mt-8">My New Work</h2>
+        <div className="p-10 flex flex-wrap -mx-4 ">
+        {articles.map((article) => 
+         <div className="w-full max-w-full mb-8 sm:w-1/2 px-4 lg:w-1/3 flex flex-col">
+         <img src={article.articleImageLink} alt="Card img" className="object-cover object-center w-full h-48" />
+         <div className="flex flex-grow">
+             <div className="triangle"></div>
+             <div className="flex flex-col justify-between px-4 py-6 bg-white border border-gray-400 text">
+                 <div>
+                    
+                     <a href={"/article-screen/" + article.articleId}
+                         className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
+                         {article.articleTitle}
+                     </a>
+                 
+                 </div>
+                 <div>
+                     <a href={"/article-screen/" + article.articleId}
+                         className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
+                         More -></a>
+                 </div>
+             </div>
+         </div>
+     </div>
+        )
+        }
+    </div>
+        </div>
+    
+    </div>
+    );
+  }
